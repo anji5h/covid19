26457b88a60cee9d8b6de98f102684ca613c3b56
anji5h <anjishpanta@gmail.com>
2020-04-25 18:17:44 +0545

    new-tracker

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Covid19 from './covid19/covid19.component';
+import Approute from './approuting';
 
 function App() {
   return (
     <div className="App">
-    <Covid19></Covid19>
+    <Approute></Approute>
     </div>
   );
 }
---
+++ src/header/header.component.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './header.css'
+import {Link} from 'react-router-dom'
+export default function Header() {
+    return (
+        <div>
+            <div className="header">
+          <p>
+            COVID<span>-19</span>
+          </p>
+        </div>
+        <div className="menu">
+          <span><Link to='/home'>HOME</Link></span>
+          <span><Link to='/world'>WORLD</Link></span>
+          <span><Link to='/symptom'>SYMPTOMS</Link></span>
+        </div>
+        </div>
+    )
+}
